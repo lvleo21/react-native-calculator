5679f7a53dcf9c1dec31e5322c9e84cfc350166c
Leonardo Veras M. O. Lopes <leonardo@layoutsistemas.com.br>
2022-03-27 18:06:43 -0300

    add a readme

---
+++ readme.md
@@ -0,0 +1,10 @@
+<h1 align="center">Calculadora</h1>
+
+<p align="center">
+Este projeto foi realizado em um dos módulos do curso de React Native
+oferecidos pela Cod3r. Depois de termos uma breve apresentação dos conceitos básicos do react, partimos para o primeiro app que envolve todos os conceitos já vistos, assim como, algumas particularidades referetes a regra de negócio da calculadora.
+</p>
+
+![alt text]('https://imgur.com/a/H4Nzaaf')
+
+<img src="https://imgur.com/a/H4Nzaaf" width="350" title="hover text">
--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -28,29 +28,56 @@ class Calculator extends Component {
 
     addDigit = (value) => {
 
+        const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+
+
         //* Ele verifica se já existe um . no número digitado
-        if (value === "." && this.state.displayValue.includes(".")) {
+        if (value === "." && !clearDisplay && this.state.displayValue.includes(".")) {
             return
         }
 
-        const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
         const currentValue = clearDisplay ? '' : this.state.displayValue
         const displayValue = currentValue + value
         this.setState({ displayValue, clearDisplay: false })
 
-        if (value !== "."){
+        if (value !== ".") {
             const newValue = parseFloat(displayValue);
 
             const values = [...this.state.values]
-            values[this.state.current] = newValue
+            values[this.state.currentCursor] = newValue
 
-            this.setState({values})
+            this.setState({ values })
         }
 
     }
 
     setOperation = (operation) => {
-        ;
+
+        if (this.state.currentCursor === 0) {
+            this.setState({ operation, currentCursor: 1, clearDisplay: true })
+        } else {
+            const equals = operation === "=";
+            const values = [...this.state.values]
+            try {
+                console.log(`${values[0]} ${this.state.operation} ${values[1]}`)
+                values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`)
+            } catch (err) {
+                values[0] = this.state.values[0]
+            }
+
+            values[1] = 0
+
+            this.setState({
+                displayValue: `${values[0]}`,
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
+                values
+            })
+
+
+        }
+        console.log(this.state)
     }
 
     clearMemory = () => {
@@ -78,8 +105,8 @@ class Calculator extends Component {
                     <Button label={"3"} onClick={this.addDigit} />
                     <Button label={"+"} onClick={this.setOperation} operations />
                     <Button label={"0"} cols={2} onClick={this.addDigit} />
-                    <Button label={"."} onClick={this.addDigit}/>
-                    <Button label={"="} operations />
+                    <Button label={"."} onClick={this.addDigit} />
+                    <Button label={"="} operations onClick={this.setOperation} />
                 </View>
             </>
         )
